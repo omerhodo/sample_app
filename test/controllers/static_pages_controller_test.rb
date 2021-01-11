75c155c7718950b4c218fc5db1f8723204bfe42b
= <=>
2021-01-11 15:07:56 +0300

    exercies 5.3.1 finish

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
   test "should get help" do
     get static_pages_help_url
     assert_response :success
@@ -29,10 +35,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
 
 end
